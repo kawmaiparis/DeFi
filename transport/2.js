fd7e74e099428f083f04ab7c32d9628d21aadb75
Paris <paris.kawmai@gmail.com>
2020-02-13 14:56:14 +0000

    add protons probuff

---
+++ transport/2.js
@@ -0,0 +1,58 @@
+"use strict";
+const pipe = require("it-pipe");
+const { toBuffer } = require("it-buffer");
+
+const Libp2p = require("libp2p");
+const TCP = require("libp2p-tcp");
+const SECIO = require("libp2p-secio");
+const PeerInfo = require("peer-info");
+const MPLEX = require("libp2p-mplex");
+
+const createNode = async peerInfo => {
+    // To signall the addresses we want to be available, we use
+    // the multiaddr format, a self describable address
+    peerInfo.multiaddrs.add("/ip4/0.0.0.0/tcp/0");
+    const node = await Libp2p.create({
+        peerInfo,
+        modules: {
+            transport: [TCP],
+            connEncryption: [SECIO],
+            streamMuxer: [MPLEX]
+        }
+    });
+
+    await node.start();
+    return node;
+};
+
+function printAddrs(node, number) {
+    console.log("node %s is listening on:", number);
+    node.peerInfo.multiaddrs.forEach(ma => console.log(ma.toString()));
+}
+
+(async () => {
+    const [peerInfo1, peerInfo2] = await Promise.all([
+        PeerInfo.create(),
+        PeerInfo.create()
+    ]);
+
+    const [node1, node2] = await Promise.all([
+        createNode(peerInfo1),
+        createNode(peerInfo2)
+    ]);
+
+    printAddrs(node1, 1);
+    printAddrs(node2, 2);
+
+    node2.handle("/print", ({ stream }) => {
+        pipe(stream, async function(source) {
+            for await (const msg of source) {
+                console.log(msg.toString());
+            }
+        });
+    });
+
+    const { stream } = await node1.dialProtocol(node2.peerInfo, "/print");
+
+    await pipe(["Hello", " ", "p2p", " ", "world", "!"], stream);
+})();
